2a18920ce1e69efb26397e16178a8db008407a5a
polaris0027 <pola2272@gmail.com>
2022-04-05 23:23:04 +0900

    author(question,answer) added

--- board/models.py
+++ board/models.py
@@ -1,5 +1,6 @@
 from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200, verbose_name='질문제목')
     content = models.TextField(verbose_name='질문내용')
     create_date = models.DateTimeField(auto_now=True, verbose_name='작성일시')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.subject
@@ -16,4 +18,5 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='답변제목')
     content = models.TextField(verbose_name='답변내용')
     create_date = models.DateTimeField(auto_now=True, verbose_name='작성일시')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     
--- board/views.py
+++ board/views.py
@@ -3,6 +3,7 @@ from board.models import Answer, Question
 from django.views.generic import ListView, DetailView
 from board.forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -21,12 +22,14 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'board/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.question = question
             answer.save()
             return redirect('board:detail', question_id=question.id)
@@ -35,11 +38,13 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'board/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.save()
             return redirect('board:index')
     else:
