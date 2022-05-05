67ca93303fb4f8eaa2e55541c23a0fd3280bbf39
polaris0027 <pola2272@gmail.com>
2022-05-05 22:06:49 +0900

    language change

--- todo/models.py
+++ todo/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class TodoList(models.Model):
-    name = models.CharField(max_length=40, verbose_name="할일")
-    description = models.TextField(max_length=200, verbose_name="내용")
-    date_created = models.DateField(auto_now_add=True, verbose_name="작성일")
-    date_deadline = models.DateField(verbose_name="마감일")
-    image = models.ImageField(null=True, blank=True, upload_to="", verbose_name='이미지')
-    file = models.FileField(null=True, blank=True, upload_to="uploads/", verbose_name='파일')
+    name = models.CharField(max_length=40, verbose_name="Task")
+    description = models.TextField(max_length=200, verbose_name="Description")
+    date_created = models.DateField(auto_now_add=True, verbose_name="Date")
+    date_deadline = models.DateField(verbose_name="Deadline")
+    image = models.ImageField(null=True, blank=True, upload_to="", verbose_name='Image')
+    file = models.FileField(null=True, blank=True, upload_to="uploads/", verbose_name='File')
     author = models.ForeignKey(User, on_delete=models.CASCADE, name='author')
     
     #別のクラスを作らないでimage,fileフィルドを作ると、１枚しか添付できない。
@@ -19,12 +19,12 @@ class TodoList(models.Model):
         delta = self.date_deadline - date.today()
         days = delta.days
         if days > 0:
-            return f'{days}일 남음'
+            return f'{days}Days reamining'
         elif days == 0:
-            return '오늘까지!!'
+            return 'Do It Today!!'
         else:
             days = abs(days)
-            return f'{days}일 지남'
+            return f'{days}days passed'
         
     #残った日数を返還するためにdeltaを指定し、daysを与える。
         
